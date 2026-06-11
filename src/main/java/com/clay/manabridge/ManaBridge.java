31048730bd3d9629e3c46032e8d78a47a1a3d4c7
clay714cer <antoncernonog@gmail.com>
2026-06-11 04:04:29 +0300

    Update ManaBridge.java

--- src/main/java/com/clay/manabridge/ManaBridge.java
+++ src/main/java/com/clay/manabridge/ManaBridge.java
@@ -5,7 +5,6 @@ import com.clay.manabridge.network.ManaSyncManager;
 import com.mojang.logging.LogUtils;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.ModContainer;
-import net.neoforged.fml.ModLoadingContext;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.config.ModConfig;
 import net.neoforged.neoforge.common.NeoForge;
@@ -20,7 +19,7 @@ public class ManaBridge {
     public static final Logger LOGGER = LogUtils.getLogger();
 
     public ManaBridge(IEventBus modEventBus, ModContainer modContainer) {
-        ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, Config.SPEC);
+      modContainer.registerConfig(ModConfig.Type.COMMON, Config.SPEC);
         
         NeoForge.EVENT_BUS.addListener(this::onPlayerTick);
         NeoForge.EVENT_BUS.addListener(this::onPlayerLogin);
@@ -39,7 +38,9 @@ public class ManaBridge {
     
     private void onPlayerLogin(PlayerEvent.PlayerLoggedInEvent event) {
         if (event.getEntity().level().isClientSide()) return;
-        ManaSyncManager.updateMaxMana(event.getEntity());
+        if (event.getEntity() instanceof net.minecraft.server.level.ServerPlayer player) {
+    ManaSyncManager.updateMaxMana(player);
+}
     }
     
     private void onEffectAdded(MobEffectEvent.Added event) {
